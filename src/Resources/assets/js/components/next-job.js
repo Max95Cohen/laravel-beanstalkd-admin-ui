c7e53a01ecbf09d62f074e2c8a1a4b79e159aa3d
Max_Cohen <showman_sexy@bk.ru>
2025-05-08 12:39:43 +0500

    update next-job.js.

--- src/Resources/assets/js/components/next-job.js
+++ src/Resources/assets/js/components/next-job.js
@@ -1,6 +1,6 @@
 var NextJob = Vue.extend({
     template: '\
-        <div class="col-md-4 col-xs-12">\
+        <div class="col-xs-12">\
             <div class="x_panel">\
                 <div class="x_title">\
                     <h2><span class="{{ icon }}"></span> {{ title }}</h2>\
